5b013ad2d79fd8a2c5861d1cf774a4cb1702f291
Kaio Goes <kaio.goes@spezi.com.br>
2023-07-02 23:24:10 -0300

    Feature screen management product

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:shop/models/order_list.dart';
 import 'package:shop/models/product_list.dart';
 import 'package:shop/pages/cart_page.dart';
 import 'package:shop/pages/orders.page.dart';
+import 'package:shop/pages/products_page.dart';
 import 'package:shop/pages/products_overview_page.dart';
 import 'package:shop/pages/produtc_detail_page.dart';
 import 'package:shop/utils/app_routes.dart';
@@ -43,7 +44,8 @@ class MyApp extends StatelessWidget {
           AppRoutes.home: (ctx) => const ProdutcsOverviewPage(),
           AppRoutes.productDetail: (ctx) => const ProductDetailPage(),
           AppRoutes.cart: (ctx) => const CartPage(),
-          AppRoutes.orders: (ctx) => const OrdersPage()
+          AppRoutes.orders: (ctx) => const OrdersPage(),
+          AppRoutes.products: (ctx) => const ProductsPage()
         },
       ),
     );
--- lib/utils/app_routes.dart
+++ lib/utils/app_routes.dart
@@ -1,7 +1,7 @@
 class AppRoutes {
   static const productDetail = '/product-detail';
-  static const home =  '/';
-  static const cart =  '/cart';
-    static const orders =  '/orders';
-
+  static const home = '/';
+  static const cart = '/cart';
+  static const orders = '/orders';
+  static const products = '/products';
 }
