2759a3e8509489e7e5105a5b18e77dc4133651dc
Kaio Goes <kaio.goes@spezi.com.br>
2023-07-05 01:45:48 -0300

    Feature remove product

--- lib/components/product_item.dart
+++ lib/components/product_item.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shop/models/product.dart';
+import 'package:shop/models/product_list.dart';
 import 'package:shop/utils/app_routes.dart';
 
 class ProductItem extends StatelessWidget {
@@ -28,7 +30,30 @@ class ProductItem extends StatelessWidget {
                 color: Theme.of(context).colorScheme.primary,
               ),
               IconButton(
-                onPressed: () {},
+                onPressed: () {
+                  showDialog<bool>(
+                    context: context,
+                    builder: (ctx) => AlertDialog(
+                      title: const Text('Excluir Produto'),
+                      content: const Text('Tem certeza?'),
+                      actions: [
+                        TextButton(
+                            onPressed: () => Navigator.of(context).pop(false),
+                            child: const Text('Não')),
+                        TextButton(
+                            onPressed: () {
+                              Navigator.of(context).pop(true);
+                            },
+                            child: const Text('Sim'))
+                      ],
+                    ),
+                  ).then((value) {
+                    if (value ?? false) {
+                      Provider.of<ProductList>(context, listen: false)
+                          .removeProduct(product);
+                    }
+                  });
+                },
                 icon: const Icon(Icons.delete),
                 color: Theme.of(context).colorScheme.error,
               ),
