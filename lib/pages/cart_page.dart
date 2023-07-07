14799c4d7647d948f3b53887dcdc2cf1214aa8c4
Kaio Goes <kaio.goes@spezi.com.br>
2023-07-07 17:19:17 -0300

    Feature order in firebase

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -42,18 +42,7 @@ class CartPage extends StatelessWidget {
                                   ?.color),
                         )),
                     const Spacer(),
-                    TextButton(
-                      onPressed: () {
-                        Provider.of<OrderList>(context, listen: false)
-                            .addOrder(cart);
-
-                        cart.clear();
-                      },
-                      style: TextButton.styleFrom(
-                          textStyle: TextStyle(
-                              color: Theme.of(context).colorScheme.primary)),
-                      child: const Text("COMPRAR"),
-                    )
+                    CartButton(cart: cart)
                   ]),
             ),
           ),
@@ -67,5 +56,48 @@ class CartPage extends StatelessWidget {
         ],
       ),
     );
+  }
+}
+
+class CartButton extends StatefulWidget {
+  const CartButton({
+    super.key,
+    required this.cart,
+  });
+
+  final Cart cart;
+
+  @override
+  State<CartButton> createState() => _CartButtonState();
+}
+
+class _CartButtonState extends State<CartButton> {
+  bool isLoading = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return isLoading
+        ? const CircularProgressIndicator()
+        : TextButton(
+            onPressed: widget.cart.itemCount == 0
+                ? null
+                : () async {
+                    setState(() {
+                      isLoading = true;
+                    });
+                    await Provider.of<OrderList>(context, listen: false)
+                        .addOrder(widget.cart);
+
+                    widget.cart.clear();
+
+                    setState(() {
+                      isLoading = false;
+                    });
+                  },
+            style: TextButton.styleFrom(
+                textStyle:
+                    TextStyle(color: Theme.of(context).colorScheme.primary)),
+            child: const Text("COMPRAR"),
+          );
   }
 }
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,4 +1,6 @@
 class Constants {
   static const productBaseUrl =
       "https://shop-d97d5-default-rtdb.firebaseio.com/products";
+      static const ordertBaseUrl =
+      "https://shop-d97d5-default-rtdb.firebaseio.com/orders";
 }
