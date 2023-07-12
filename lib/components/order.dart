6f6ff65a574a7c359deaba08a910dedae6f71d4f
Kaio Goes <kaio.goes@spezi.com.br>
2023-07-12 17:21:58 -0300

    Feauture animatedContainer in order

--- lib/components/order.dart
+++ lib/components/order.dart
@@ -15,47 +15,54 @@ class _OrderWidgetState extends State<OrderWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Column(
-        children: [
-          ListTile(
-            title: Text("R\$ ${widget.order.total.toStringAsFixed(2)}"),
-            subtitle:
-                Text(DateFormat('dd/MM/yyyy hh:mm').format(widget.order.date)),
-            trailing: IconButton(
-              icon: const Icon(Icons.expand_more),
-              onPressed: () {
-                setState(() {
-                  _expanded = !_expanded;
-                });
-              },
-            ),
-          ),
-          if (_expanded)
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
-              height: (widget.order.products.length * 25.0) + 15,
-              child: ListView(
-                children: widget.order.products.map((product) {
-                  return Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        product.name,
-                        style: const TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.bold),
-                      ),
-                      Text(
-                        '${product.quantity}x R\$ ${product.price}',
-                        style:
-                            const TextStyle(fontSize: 18, color: Colors.grey),
-                      )
-                    ],
-                  );
-                }).toList(),
+    final itemsHeight = (widget.order.products.length * 25.0) + 15;
+
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 300),
+      height: _expanded ? itemsHeight + 80 : 80,
+      child: Card(
+        child: Column(
+          children: [
+            ListTile(
+              title: Text("R\$ ${widget.order.total.toStringAsFixed(2)}"),
+              subtitle: Text(
+                  DateFormat('dd/MM/yyyy hh:mm').format(widget.order.date)),
+              trailing: IconButton(
+                icon: const Icon(Icons.expand_more),
+                onPressed: () {
+                  setState(() {
+                    _expanded = !_expanded;
+                  });
+                },
               ),
-            )
-        ],
+            ),
+              AnimatedContainer(
+                duration: const Duration(milliseconds: 300),
+                height: _expanded ? itemsHeight : 0,
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+                child: ListView(
+                  children: widget.order.products.map((product) {
+                    return Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          product.name,
+                          style: const TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          '${product.quantity}x R\$ ${product.price}',
+                          style:
+                              const TextStyle(fontSize: 18, color: Colors.grey),
+                        )
+                      ],
+                    );
+                  }).toList(),
+                ),
+              )
+          ],
+        ),
       ),
     );
   }
