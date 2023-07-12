db3e2634af06bda6299ce390ec7f6c7d499fb8c7
Kaio Goes <kaio.goes@spezi.com.br>
2023-07-12 17:41:09 -0300

    Feature navigate in CustomPage

--- lib/components/app_drawer.dart
+++ lib/components/app_drawer.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop/models/auth.dart';
+import 'package:shop/pages/orders.page.dart';
 import 'package:shop/utils/app_routes.dart';
+import 'package:shop/utils/custom_route.dart';
 
 class AppDrawer extends StatelessWidget {
   const AppDrawer({super.key});
@@ -29,6 +31,7 @@ class AppDrawer extends StatelessWidget {
             title: const Text('Pedidos'),
             onTap: () {
               Navigator.of(context).pushReplacementNamed(AppRoutes.orders);
+              // Navigator.of(context).pushReplacement(CustomRoute(builder: (ctx) => const OrdersPage()));
             },
           ),
           const Divider(),
